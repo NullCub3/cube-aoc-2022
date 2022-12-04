f7545d2d8224166bfee41d3638d0dafecdd4f853
NullCube <51034487+NullCub3@users.noreply.github.com>
2022-12-03 18:34:35 -0800

    Part 1 Complete

--- day-1/main.py
+++ day-1/main.py
@@ -1,1 +1,16 @@
 
+elves = list()
+calc = 0
+
+with open('input.txt') as f:
+    for line in f:
+        e = line.strip()
+        if e:
+            calc += int(e)
+        else:
+            elves.append(calc)
+            calc = 0
+
+elves.sort(reverse=True)
+
+print(elves[0])
